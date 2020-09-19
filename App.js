91743befcdb81bdb126d06de2c6c7f95801558c4
HotSpor <hotspor00@gmail.com>
2020-09-20 01:26:38 +0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [lista, setLista] = React.useState([]);
+  const [task, setTask] = React.useState("");
+
+
+  const buttonPressedAdd = () => {
+    setLista([...lista, task])
+    setTask("")
+  }
+
+  const buttonPressedClear = () => {
+    setLista([])
+  }
+
+  const textChanged = text => {
+    setTask(text)
+  }
+
+
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.textInput} onChangeText={text => textChanged(text)} value={task}></TextInput>
+        <View style={styles.buttons}>
+          <Button onPress={buttonPressedAdd} title="Add"></Button>
+          <Button title="Clear" onPress={buttonPressedClear}></Button>
+        </View>
+        <View style={styles.flexBox}>
+          <FlatList
+            data={lista}
+            keyExtractor={(item,index) => index.toString()}
+            renderItem={({ item }) => (
+              <Text>{item}</Text>
+            )} />
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection:"column",
+      alignItems: 'stretch',
+      justifyContent: "center",
+    },
+    buttons: {
+      flexDirection: "row",
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: "space-around",
+    },
+    textInput: {
+      borderWidth: 1,
+      marginHorizontal:100,
+      marginBottom:30,
+      justifyContent:"center"
+    }, 
+    flexBox: {
+      alignItems:"center",
+      justifyContent:"center"
+    }
+  });
